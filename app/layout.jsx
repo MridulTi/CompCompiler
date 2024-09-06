0204ef717c159716b435076f5a1f49387fea77a7
= <mridultiwari2002@gmail.com>
2024-09-06 06:47:15 +0530

    added navbar and google signup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,10 +13,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <main>
-          {children}
-        </main>
+      <body>
+        <Provider>
+          <main>
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid place-items-center">Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="text-center font-bold text-5xl tracking-tight">Participate in Coding Competition</h1>
+      <h2></h2>
+    </section>
   );
 }
